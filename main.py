61cffbacbab4623a2e0b9d3b4e988ef87ba3be21
dekxrma <nazar.digtiarenko@gmail.com>
2025-09-24 10:27:16 +0300

    minor improvements + bugs

--- main.py
+++ main.py
@@ -1,105 +1,129 @@
 import dearpygui.dearpygui as dpg
-
+import time
 dpg.create_context()
 """
-
-Створити байнері трі з нодів. В функції ран отримати звязки між ними. Корнем (кінцевою нодою) буде той хто не має батька. всі інші матимуть. 
-Звязки виглядають так
-(29, 33)
-(33, 42)
-(47, 33)
-
-лівий елемент це дитина, а правий батько. Незалежно від порядку створення звязків, нода 42 нематиме батька. Після побудови байнері трі, треба починати обчислення з неї. 
-Можна буде відразу проставляти звязки в класи. 
-
+баг з трьома арефметичними діями
 """
 
 class InputNode:
     def spawn(self):
-        with dpg.node(label="Number input", parent="editor", user_data=self):
+        with dpg.node(label="Number input", parent="editor", user_data=self) as f:
+            self.f = f
             with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Output):
                 self.inp = dpg.add_input_float(width=150)
                 dpg.add_text(dpg.get_item_parent(dpg.get_item_parent(self.inp)))
     
     def calculate(self):
+        dpg.set_item_label(self.f, "HERE")
+        time.sleep(1)
+        dpg.set_item_label(self.f, "Number input")
         return dpg.get_value(self.inp)
         
 class SumNode:
     def __init__(self):
-        self.A1: InputNode = None
-        self.A2: InputNode = None
+        self.A1: int = None
+        self.A2: int = None
         
     def spawn(self):
-        with dpg.node(label="Number input", parent="editor", user_data=self):
-            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input):
-                self.a = dpg.add_text("First digit")
-            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input):
+        with dpg.node(label="Summa", parent="editor", user_data=self) as f:
+            self.f = f
+            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input) as fd:
+                self.A1 = fd
+                dpg.add_text("First digit")
+            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input) as sd:
+                self.A2 = sd
                 dpg.add_text("Second digit")
             
             with dpg.node_attribute(label="Float Output", attribute_type=dpg.mvNode_Attr_Output):
                 self.r = dpg.add_text("Result")
-                dpg.add_text(dpg.get_item_parent(dpg.get_item_parent(self.a)))
+                dpg.add_text(dpg.get_item_parent(dpg.get_item_parent(self.r)))
     
-    def set_children(self, children):
-        if not self.A1:
-            self.A1 = children
-        else:
-            self.A2 = children
+    # def set_children(self, children):
+    #     if not self.A1:
+    #         self.A1 = children
+    #     else:
+    #         self.A2 = children
     
     def calculate(self):
-        dpg.set_value(item = self.r, value = f"Result: {self.A1.calculate() + self.A2.calculate()}")
-        return self.A1.calculate() + self.A2.calculate()
+        dpg.set_item_label(self.f, "HERE")
+        res = dpg.get_item_user_data(self.A1).calculate() + dpg.get_item_user_data(self.A2).calculate()
+        dpg.set_value(item = self.r, value = f"Result: {res}")
+        time.sleep(1)
+        dpg.set_item_label(self.f, "Summa")
+        return res
 
 class MulNode:
     def __init__(self):
-        self.A1: InputNode = None
-        self.A2: InputNode = None
+        self.A1: int = None
+        self.A2: int = None
         
     def spawn(self):
-        with dpg.node(label="Number input", parent="editor", user_data=self):
-            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input):
-                self.a = dpg.add_text("First digit")
-            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input):
+        with dpg.node(label="Multiply", parent="editor", user_data=self) as f:
+            self.f = f
+            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input) as fd:
+                self.A1 = fd
+                dpg.add_text("First digit")
+            with dpg.node_attribute(label="Float input", attribute_type=dpg.mvNode_Attr_Input) as sd:
+                self.A2 = sd
                 dpg.add_text("Second digit")
             
             with dpg.node_attribute(label="Float Output", attribute_type=dpg.mvNode_Attr_Output):
                 self.r = dpg.add_text("Result")
-                dpg.add_text(dpg.get_item_parent(dpg.get_item_parent(self.a)))
+                dpg.add_text(dpg.get_item_parent(dpg.get_item_parent(self.r)))
     
-    def set_children(self, children):
-        if not self.A1:
-            self.A1 = children
-        else:
-            self.A2 = children
+    # def set_children(self, children):
+    #     if not self.A1:
+    #         self.A1 = children
+    #     else:
+    #         self.A2 = children
     
     def calculate(self):
-        dpg.set_value(item = self.r, value = f"Result: {self.A1.calculate() * self.A2.calculate()}")
-        return self.A1.calculate() * self.A2.calculate()
+        print(self.A1, self.A2)
+        print(self)
+        dpg.set_item_label(self.f, "HERE")
+        res = dpg.get_item_user_data(self.A1).calculate() * dpg.get_item_user_data(self.A2).calculate()
+        dpg.set_value(item = self.r, value = f"Result: {res}")
+        time.sleep(1)
+        dpg.set_item_label(self.f, "Multiply")
+        return res
 
 class OutNode:
     def __init__(self):
-        self.A1: InputNode = None
+        self.A1: int = None
         
     def spawn(self):
-        with dpg.node(label="Output", parent="editor", user_data=self):
-            with dpg.node_attribute(label="Output"):
+        with dpg.node(label="Output", parent="editor", user_data=self) as f:
+            self.f = f
+            with dpg.node_attribute(label="Output") as out:
+                self.A1 = out
                 self.txt = dpg.add_text("output")
+                # self.txt = dpg.add_text(self.A1)
                 dpg.add_text(dpg.get_item_parent(dpg.get_item_parent(self.txt)))
                 
-    def set_children(self, children):
-        self.A1 = children
+    # def set_children(self, children):
+    #     self.A1 = children
             
     def calculate(self):
-        dpg.set_value(item = self.txt, value = f"Result: {self.A1.calculate()}")
+        dpg.set_item_label(self.f, "HERE")
+        dpg.set_value(item = self.txt, value = f"Result: {dpg.get_item_user_data(self.A1).calculate()}")
+        time.sleep(1)
+        dpg.set_item_label(self.f, "Output")
         
-
+def open_modal_callback(text: str, name:str="Error"):
+    with dpg.window(label=name, modal=True, show=False, tag="modal_window"):
+        dpg.add_text(text)
+        dpg.add_button(label="Close", callback=lambda: dpg.delete_item("modal_window"))
+    dpg.show_item("modal_window")
 
 # callback runs when user attempts to connect attributes
 def link_callback(sender, app_data):
     # app_data -> (link_id1, link_id2)
-    dpg.add_node_link(app_data[0], app_data[1], parent=sender, user_data=(dpg.get_item_parent(app_data[0]), dpg.get_item_parent(app_data[1])))
+    left_parent = dpg.get_item_parent(app_data[0])
+    dpg.add_node_link(app_data[0], app_data[1], parent=sender, user_data=left_parent)
+    dpg.set_item_user_data(app_data[1], dpg.get_item_user_data(left_parent))
     print(sender)
     print(app_data)
+    print("^^^")
     
 def call(sender, app_data):
     print(sender)
@@ -109,15 +133,19 @@ def run_callback(sender, app_data, user_data):
     print(dpg.get_item_children("editor"))
     nodes: list = dpg.get_item_children("editor")[1]
     for x in dpg.get_item_children("editor")[0]:
-        lid = dpg.get_item_user_data(x)[0]
-        l: InputNode = dpg.get_item_user_data(lid)
-        r: OutNode = dpg.get_item_user_data(dpg.get_item_user_data(x)[1])
+        # print(lid)
+        # left: InputNode = dpg.get_item_user_data(lid)
+        # right: OutNode = dpg.get_item_user_data(x)[1]
         
-        r.set_children(l)
-        nodes.remove(lid)
+        # right.set_children(left)
+        nodes.remove(dpg.get_item_user_data(x))
     
-    dpg.get_item_user_data(nodes[0]).calculate()
-        
+    if len(nodes) == 1:
+        dpg.get_item_user_data(nodes[0]).calculate()
+    elif len(nodes) >= 2:
+        open_modal_callback("Run is not possible due to 2 or more chains detected. Remove all unnecessary chains.")
+    else:
+        open_modal_callback("Run is not possible because no chains have been detected.")
 
 
 # callback runs when user attempts to disconnect attributes
